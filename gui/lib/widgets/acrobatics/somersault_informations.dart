d4acd502dc1f1761a322f41b08d372b09f719294
Alexandre WENG <alexandre.weng@epita.fr>
2023-10-18 12:03:51 -0400

    wip

--- gui/lib/widgets/acrobatics/somersault_informations.dart
+++ gui/lib/widgets/acrobatics/somersault_informations.dart
@@ -1,4 +1,3 @@
-import 'package:bioptim_gui/models/acrobatics_ocp_controllers.dart';
 import 'package:bioptim_gui/widgets/utils/positive_integer_text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';

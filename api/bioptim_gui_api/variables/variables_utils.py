47d69d187a12608fae3d77c67e6db4c67c52eb69
Alexandre WENG <alexandre.weng@epita.fr>
2023-10-30 10:50:46 -0400

    wip: refactor

---
+++ api/bioptim_gui_api/variables/variables_utils.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def variables_zeros(dimension: int, interpolation_type: str) -> list:
+    """
+    Return a zero array with the appropriate dimension depending on the interpolation type
+
+    Parameters
+    ----------
+    dimension: int
+        The dimension of the variable
+    interpolation_type: str
+        The type of interpolation ("LINEAR", "CONSTANT", "CONSTANT_WITH_FIRST_AND_LAST_DIFFERENT")
+
+    Returns
+    -------
+    The array of zeros
+    """
+    if interpolation_type == "LINEAR":
+        return np.zeros((dimension, 2)).tolist()
+    elif interpolation_type == "CONSTANT":
+        return np.zeros((dimension, 1)).tolist()
+    else:
+        return np.zeros((dimension, 3)).tolist()
